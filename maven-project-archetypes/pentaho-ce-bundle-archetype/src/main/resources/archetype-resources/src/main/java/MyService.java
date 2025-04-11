df0ac58ce70769926623a9a90c87fc50bb147507
Ing. Óscar Bonilla, MBA <obonilla66@soin.co.cr>
2025-04-11 12:47:04 -0600

    Unificación con subtree de todos los repositorios Pentaho 10.1 en el repositorio pentaho_10.1

--- maven-project-archetypes/pentaho-ce-bundle-archetype/src/main/resources/archetype-resources/src/main/java/MyService.java
+++
@@ -1,7 +0,0 @@
-package ${package};
-
-public interface MyService {
-
-  String sayHello();
-
-}
--- maven-project-archetypes/pentaho-streaming-step-plugin-archetype/src/main/resources/archetype-resources/src/test/java/__plugin_class_name__Test.java
+++
@@ -1,7 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package};
-
-public class ${plugin_class_name}Test {
-}
